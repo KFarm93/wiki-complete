47c728239fcf0a133391dfc8e9fa20f248a65a18
Kevin Farmer <farmekp0@sewanee.edu>
2016-10-17 16:55:58 -0400

    Committed with gitignore and wsgi.

--- server.py
+++ server.py
@@ -1,10 +1,20 @@
+from dotenv import load_dotenv, find_dotenv
+load_dotenv(find_dotenv())
+
 from flask import Flask, render_template, redirect, request, session, flash
 import pg
 from wiki_linkify import wiki_linkify
+import os
 
-db = pg.DB(dbname='wiki_db')
-app = Flask('Wiki')
-app.secret_key = '12345'
+db = pg.DB(
+    dbname=os.environ.get('PG_DBNAME'),
+    host=os.environ.get('PG_HOST'),
+    user=os.environ.get('PG_USERNAME'),
+    passwd=os.environ.get('PG_PASSWORD')
+)
+
+tmp_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates')
+app = Flask('wiki', template_folder=tmp_dir)
 
 @app.route('/')
 def home():
